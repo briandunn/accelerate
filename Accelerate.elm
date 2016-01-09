f1e81771099549bec885ba13376a6bc66be79a0f
Brian Dunn and Dorian Karter <dev+briandunn+dkarter@hashrocket.com>
2016-01-08 23:39:08 -0600

    Add a bouncy red dot

--- Accelerate.elm
+++ Accelerate.elm
@@ -1,12 +1,13 @@
 module Accelerate where
 
 -- IMPORTS
-import Html exposing (..)
-import Effects
-import String
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Color
 import Signal
 import Time
-import StartApp as StartApp
+import Text
+import Window
 
 type alias Point = {x:Float, y:Float, z:Float}
 type alias Note = Int
@@ -42,18 +43,26 @@ update action model =
 
 pointToNote : Point -> Note
 pointToNote point =
-  round(200 + abs (point.x * 300))
+  round(200 + abs ((point.x + point.y + point.z) * 100))
 
 -- VIEW
 
-view address model =
-  div [] [
-    text <| toString model.currentPoint.x
-  ]
+view : (Int,Int) -> Model -> Element
+view (w,h) model =
+  let
+    c = circle ( model.currentPoint.z * (toFloat w))
+      |> filled Color.red
+      |> move (model.currentPoint.x * (toFloat w), model.currentPoint.y * (toFloat h))
+  in
+    collage w h [c]
 
 inbox : Signal.Mailbox Action
 inbox =
   Signal.mailbox NoOp
+
+actions : Signal Action
+actions =
+  Signal.merge inbox.signal (Signal.map AddPoint sampledAcceleration)
 
 state : Signal Model
 state =
@@ -63,9 +72,7 @@ sampledAcceleration : Signal Point
 sampledAcceleration =
   Signal.sampleOn (Time.fps 30) acceleration
 
-actions : Signal Action
-actions =
-  Signal.merge inbox.signal (Signal.map AddPoint sampledAcceleration)
-
+main : Signal Element
 main =
-  Signal.map (view inbox.address) state
+    Signal.map2 view Window.dimensions state
+
